a0574d7f16c6b6e0d42a7304b0a4fddb6577f5a8
Joachim Mwengi <kimwambua96@gmail.com>
2024-02-06 23:59:05 +0300

    implemented about us page sections

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -1,14 +1,16 @@
 import WhoWeAre from "../sections/WhoWeAre";
 import CallToAction from "../sections/CallToAction";
 import CoreValues from "../sections/CoreValues";
+import Footer from "../sections/Footer";
 
 const AboutUs = () => {
   return (
     <>
-      {/* TODO: Add Header */}
+      {/* TODO: Add Header  */}
       <WhoWeAre />
       <CoreValues />
       <CallToAction />
+      <Footer />
     </>
   );
 };
