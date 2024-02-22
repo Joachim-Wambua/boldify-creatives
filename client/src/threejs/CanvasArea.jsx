b0afda17abfc7b3d027927cb41cd9223b1891caf
Joachim Mwengi <kimwambua96@gmail.com>
2024-02-23 01:17:37 +0300

    updated boldify 3d model

--- client/src/threejs/CanvasArea.jsx
+++ client/src/threejs/CanvasArea.jsx
@@ -1,33 +1,47 @@
 import { OrbitControls } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { useMemo, useRef } from "react";
-// import './scene.css';
+import { Canvas } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { ContactShadows, Environment } from "@react-three/drei";
+import { useLoader } from "@react-three/fiber";
+import * as THREE from "three";
 
-const BasicParticles = () => {
-  // This reference gives us direct access to our points
-  const points = useRef();
+const Scene = () => {
+  const gltf = useLoader(GLTFLoader, "/boldify.gltf");
 
-  // You can see that, like our mesh, points also takes a geometry and a material,
-  // but a specific material => pointsMaterial
-  return (
-    <points ref={points}>
-      <sphereGeometry args={[1, 48, 48]} />
-      <pointsMaterial color="#000" size={0.018} sizeAttenuation />
-    </points>
-  );
+  // Apply glossy metallic material to the model
+  gltf.scene.traverse((child) => {
+    if (child.isMesh) {
+      child.material = new THREE.MeshStandardMaterial({
+        color: 0x333333, // color
+        metalness: 1.2, // 100% metallic
+        roughness: 0.2, // Slightly rough surface
+      });
+    }
+  });
+
+  gltf.scene.scale.set(0.04, 0.04, 0.04);
+  gltf.scene.position.set(0, 0, 0);
+  return <primitive object={gltf.scene} />;
 };
 
 const CanvasArea = () => {
   return (
-    <>
-      <div className="canvas-three flex justify-center items-center">
-        <Canvas camera={{ position: [1.5, 1.5, 1.5] }}>
-          <ambientLight intensity={0.5} />
-          <BasicParticles />
-          <OrbitControls enableZoom={false} enablePan={false} autoRotate />
-        </Canvas>
-      </div>
-    </>
+    <div className="canvas-three flex justify-center items-center">
+      <Canvas camera={{ position: [5, 1.5, 1.5] }}>
+        {/* <ambientLight intensity={0.5} /> */}
+
+        <Scene />
+        <ContactShadows
+          position={[0, -2.7, 0]}
+          opacity={0.65}
+          scale={40}
+          blur={1}
+          far={9}
+        />
+        <Environment preset="studio" />
+        <OrbitControls enableZoom={false} enablePan={false} autoRotate />
+      </Canvas>
+    </div>
   );
 };
 
