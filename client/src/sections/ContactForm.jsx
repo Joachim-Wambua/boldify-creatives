931ae63c92569913dd325b0d378b15e7193b463e
Joachim Mwengi <kimwambua96@gmail.com>
2024-03-01 09:32:50 +0300

    finalised contact page content layout

--- client/src/sections/ContactForm.jsx
+++ client/src/sections/ContactForm.jsx
@@ -1,20 +1,41 @@
+import Button from "../components/Button";
+import {
+  MdLocalPhone,
+  MdLocationOn,
+  MdOutlineMailOutline,
+} from "react-icons/md";
+
 const ContactForm = () => {
   return (
     <>
       <section className="mb-32">
         <div className="relative h-[300px] overflow-hidden bg-cover bg-[50%] bg-no-repeat bg-[url('https://mdbcdn.b-cdn.net/img/new/textures/full/171.jpg')]"></div>
-        <div className="container px-6 md:px-12">
+        <div className="container mx-auto flex px-6 md:px-12">
           <div className="block rounded-lg bg-[hsla(0,0%,100%,0.7)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-[#f2f2f2] dark:shadow-black/20 md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px]">
             <div className="mb-12 grid gap-x-6 md:grid-cols-2 lg:grid-cols-3">
               {/* Contacts */}
+              <div className="mb-6 flex items-center justify-center lg:mb-0">
+                <MdLocalPhone className="mx-2" fontSize={18} />
+                <p>+254 712 345 678</p>
+              </div>
+
+              <div className="mb-6 flex items-center justify-center lg:mb-0">
+                <MdOutlineMailOutline className="mx-2" fontSize={18} />
+                <p>boldifycreatives@gmail.com</p>
+              </div>
+
+              <div className="mb-6 flex items-center justify-center lg:mb-0">
+                <MdLocationOn className="mx-2" fontSize={18} />
+                <p>Nairobi, Kenya</p>
+              </div>
             </div>
-            <div className="mx-auto max-w-[700px]">
+            <div className="form-card mx-auto max-w-[700px]">
               <form>
                 {/* Name Input */}
                 <div className="relative mb-6" data-te-input-wrapper-init>
                   <input
                     type="text"
-                    className="peer block min-h-[auto] w-full rounded border-2 border-gray-300 bg-white placeholder-slate-700 py-[0.45rem] px-3 leading-[1.6]  transition-all duration-200 ease-linear focus:placeholder-opacity-100 peer-focus:text-primary peer-focus:border-primary peer-focus:outline-none dark:text-gray-300 dark:placeholder-gray-300 dark:border-gray-600 dark:peer-focus:text-black dark:peer-focus:border-primary dark:focus:placeholder-opacity-100 [&:not([data-te-input-placeholder-active])]:placeholder-opacity-50"
+                    className="peer block min-h-[auto] w-full rounded border-2 border-gray-300 bg-white placeholder-slate-700 py-[0.45rem] px-3 leading-[1.6]  transition-all duration-200 ease-linear focus:placeholder-opacity-100 peer-focus:text-primary peer-focus:border-primary peer-focus:outline-none dark:text-slate-800 dark:placeholder-slate-600 dark:border-gray-600 dark:peer-focus:text-black dark:peer-focus:border-primary dark:focus:placeholder-opacity-100 [&:not([data-te-input-placeholder-active])]:placeholder-opacity-50"
                     id="exampleInput90"
                     placeholder="Name"
                   />
@@ -23,47 +44,25 @@ const ContactForm = () => {
                 <div className="relative mb-6" data-te-input-wrapper-init>
                   <input
                     type="email"
-                    className="peer block min-h-[auto] w-full border border-gray-300 text-slate-600 rounded bg-white py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder-opacity-100 peer-focus:text-primary peer-focus:border-primary peer-focus:outline-none dark:text-gray-300 dark:border-gray-600 dark:bg-gray-100 dark:peer-focus:text-black dark:peer-focus:border-primary dark:focus:placeholder-opacity-100 [&:not([data-te-input-placeholder-active])]:placeholder-opacity-50"
+                    className="peer block min-h-[auto] w-full rounded border-2 border-gray-300 bg-white placeholder-slate-700 py-[0.45rem] px-3 leading-[1.6]  transition-all duration-200 ease-linear focus:placeholder-opacity-100 peer-focus:text-primary peer-focus:border-primary peer-focus:outline-none dark:text-slate-800 dark:placeholder-slate-600 dark:border-gray-600 dark:peer-focus:text-black dark:peer-focus:border-primary dark:focus:placeholder-opacity-100 [&:not([data-te-input-placeholder-active])]:placeholder-opacity-50"
                     id="exampleInput91"
                     placeholder="Email address"
                   />
-                  
                 </div>
                 {/* Message Input */}
                 <div className="relative mb-6" data-te-input-wrapper-init>
                   <textarea
-                    className="peer block min-h-[auto] w-full border border-gray-300 rounded bg-white py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder-opacity-100 data-[te-input-state-active]:placeholder-opacity-100 motion-reduce:transition-none dark:text-gray-300 dark:placeholder-gray-300 dark:border-gray-600 dark:bg-gray-100 dark:peer-focus:text-black dark:peer-focus:border-primary dark:focus:placeholder-opacity-100 [&:not([data-te-input-placeholder-active])]:placeholder-opacity-50"
+                    className="peer block min-h-[auto] w-full rounded border-2 border-gray-300 bg-white placeholder-slate-700 py-[0.45rem] px-3 leading-[1.6]  transition-all duration-200 ease-linear focus:placeholder-opacity-100 peer-focus:text-primary peer-focus:border-primary peer-focus:outline-none dark:text-slate-800 dark:placeholder-slate-600 dark:border-gray-600 dark:peer-focus:text-black dark:peer-focus:border-primary dark:focus:placeholder-opacity-100 [&:not([data-te-input-placeholder-active])]:placeholder-opacity-50"
                     id="exampleFormControlTextarea1"
                     rows="3"
                     placeholder="Your Message"
                   ></textarea>
-                  
                 </div>
                 {/* Checkbox */}
                 <div className="mb-6 inline-block min-h-[1.5rem] justify-center pl-[1.5rem] md:flex">
-                  <input
-                    className="relative float-left mt-[0.15rem] mr-[6px] -ml-[1.5rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-gray-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:ml-[0.25rem] checked:after:-mt-px checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0 checked:after:border-l-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:ml-[0.25rem] checked:focus:after:-mt-px checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-t-0 checked:focus:after:border-l-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-gray-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
-                    type="checkbox"
-                    value=""
-                    id="exampleCheck96"
-                    checked
-                  />
-                  <label
-                    className="inline-block pl-[0.15rem] hover:cursor-pointer"
-                    htmlFor="exampleCheck96"
-                  >
-                    Send me a copy of this message
-                  </label>
+                  {/* Submit Button */}
+                  <Button content="SEND" />
                 </div>
-                {/* Submit Button */}
-                <button
-                  type="button"
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                  className="inline-block w-full rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] lg:mb-0"
-                >
-                  Send
-                </button>
               </form>
             </div>
           </div>
